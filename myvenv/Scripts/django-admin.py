5d0b9d1c2dc93e281d5cc91a88b00e427daaf4ad
shikou <shikou.pc@outlook.jp>
2020-12-28 18:02:52 +0900

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owner\新しいフォルダー\documents\プログラミング学習\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
